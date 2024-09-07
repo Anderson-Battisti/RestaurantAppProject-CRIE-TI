3c32321db3aefbb4d159398aa8b6ae322c9aca15
Anderson Battisti <anderson.battisti@universo.univates.br>
2024-09-07 19:12:42 -0300

    Login System added, Login API added and Menu screen (mainpage HTML/CSS) created

--- backend/LoginAPI.ts
+++ backend/LoginAPI.ts
@@ -1,14 +1,35 @@
-class Login
-{
-    login : string = "";
-    password : string = "";
-}
+import {Login} from "./classes/Login";
+import express, {Express, Request, Response} from "express";
+import cors from "cors";
+import { ExitStatus } from "typescript";
+
+let server: Express = express();
+const serverPort = 4000;
+
+server.use(cors());
+server.use(express.json());
 
 let registeredUsers: Login[] = [];
+registeredUsers[0] = {login : "admin", password : "admin"};
 
-registeredUsers[0] = 
+server.post("/checkLogin", async function(req: Request, res: Response): Promise<Response>
 {
-    login : "admin",
-    password : "admin"
-};
+    let login = req.body.login;
+    let password = req.body.password
 
+    for (let i = 0; i < registeredUsers.length; i++)
+    {
+        let registeredLogin: Login = registeredUsers[i];
+
+        if (login === registeredLogin.login && password === registeredLogin.password)
+        {
+            return res.status(200).json({success: true, message: "Usuário e senha batem. Login efetuado com sucesso!"});
+        }
+    }
+    return res.status(401).json({success: false, message: "Usuário ou senha incorretos. Falha ao efetuar login"});
+});
+
+server.listen(serverPort, () =>
+{
+    console.log("Server started on port " + serverPort);
+});
---
+++ frontend/javascript/login.js
@@ -0,0 +1,29 @@
+const apiUrl = "http://localhost:4000";
+
+async function logIn()
+{
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const login = JSON.stringify
+    ({
+        login: document.getElementById("loginEmailField").value,
+        password: document.getElementById("loginPasswordField").value 
+    });
+
+    const requestOptions = 
+    {
+        method: "POST",
+        headers: myHeaders,
+        body: login,
+        redirect: "follow"
+    };
+
+    let result = await fetch(apiUrl + "/checkLogin", requestOptions);
+    let resultJson = await result.json();
+
+    if (resultJson.success == true)
+    {
+        window.location = "inicio.html";
+    }
+}
