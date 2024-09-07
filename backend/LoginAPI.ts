0d726fe1691000ae14361d9ce777e06f2a66e0d6
Anderson Battisti <anderson.battisti@universo.univates.br>
2024-09-07 13:22:11 -0300

    Login screen (HTML/CSS) created

---
+++ backend/LoginAPI.ts
@@ -0,0 +1,14 @@
+class Login
+{
+    login : string = "";
+    password : string = "";
+}
+
+let registeredUsers: Login[] = [];
+
+registeredUsers[0] = 
+{
+    login : "admin",
+    password : "admin"
+};
+
---
+++ backend/PaymentMethod.ts
@@ -0,0 +1,7 @@
+export class PaymentMethod
+{
+    id : Number = 0;
+    name : string = "";
+    method : string = ""; // card, transference, etc
+    type : string = ""; //credit, debit, pix, 
+}
